cb9460516dd4def69b49d4352ec45a51c6e51557
Reywam <reywam@mail.ru>
2018-02-02 16:01:38 +0300

    Добавлена распарралелленная версия программы

--- lw1/Lelekov_Alexey/MonteCarloPi/MonteCarloPi/MonteCarloPi.cpp
+++ lw1/Lelekov_Alexey/MonteCarloPi/MonteCarloPi/MonteCarloPi.cpp
@@ -1,7 +1,11 @@
 #include "stdafx.h"
 #include <iostream>
+#include <vector>
 #include <string>
 #include <time.h>
+
+#include <thread>
+#include <mutex>
 
 const size_t ARGS_COUNT = 3;
 
@@ -28,42 +32,71 @@ double GenerateRandomDouble(const double &min, const double &max)
 
 Point GenerateRandomPoint(size_t squareSize)
 {
-	double min = -(double)(squareSize / 2);
 	double max = (double)(squareSize / 2);
+	double min = -max;
 	return Point(GenerateRandomDouble(min, max), GenerateRandomDouble(min, max));
 }
 
-int main(int argc, char *argv[])
-{
-	clock_t start;
-	clock_t end;
-
-	start = clock();
-	if (argc != ARGS_COUNT) {
-		cout << "Invalid count of arguements." << endl;
-		cout<< "Use: MonteCarloPi.exe <iterationsCount> <threadsCount>." << endl;
-		return 1;
-	}
-
-	size_t iterationsCount = atoi(argv[1]);	
-	size_t threadsCount = atoi(argv[2]);
-
-	size_t pointsInCircle = 0;
-
-	for(size_t i = 0; i < iterationsCount; i++) 
+void GeneratePoints(size_t &pointsInCircle, const size_t &iterationsCount, mutex &mtx) {
+	for (size_t i = 0; i < iterationsCount; i++)
 	{
+		std::lock_guard<std::mutex> lock(mtx);
 		Point point = GenerateRandomPoint(SQUARE_SIDE);
 		if (point.x * point.x + point.y * point.y <= R)
 		{
 			pointsInCircle++;
 		}
 	}
+}
+
+vector<thread> InitThreads(
+	const size_t &threadsCount, 
+	size_t &pointsInCircle,
+	const size_t &itersForThread,
+	mutex &mtx)
+{
+	vector<thread> threads;
+	for (size_t i = 0; i < threadsCount; i++)
+	{
+		threads.push_back(thread(GeneratePoints, ref(pointsInCircle), ref(itersForThread), ref(mtx)));
+	}
+	return threads;
+}
+
+void StartThreadsEndWaiting(vector<thread> &threads) 
+{
+	for (auto &thread : threads)
+	{
+		thread.join();
+	}
+}
+
+int main(int argc, char *argv[])
+{		
+	if (argc != ARGS_COUNT) {
+		cout << "Invalid count of arguements." << endl;
+		cout << "Use: MonteCarloPi.exe <iterationsCount> <threadsCount>." << endl;
+		return 1;
+	}
+
+	clock_t start = clock();
+	
+	size_t iterationsCount = atoi(argv[1]);	
+	size_t threadsCount = atoi(argv[2]);	
+	size_t itersForThread = iterationsCount / threadsCount;
+
+	size_t pointsInCircle = 0;	
+
+	mutex mtx;
+	vector<thread> threads = InitThreads(threadsCount, pointsInCircle, itersForThread, mtx);	
+	StartThreadsEndWaiting(threads);
 
 	double pi = multCoeff * pointsInCircle / iterationsCount;
 	cout << pi << endl;
-	end = clock();
+
+	clock_t end = clock();
 
 	float duration = (float)(end - start) / CLOCKS_PER_SEC;
-	cout << "Time: " << duration << endl;
+	cout << "Time: " << duration << endl;	
     return 0;
 }
