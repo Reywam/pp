386684c00526ad3da5c88d07ee15806a798ab9c5
Reywam <reywam@mail.ru>
2018-02-02 12:37:56 +0300

    Добавлен последовательный вариант программы

---
+++ lw1/Lelekov_Alexey/MonteCarloPi/MonteCarloPi/MonteCarloPi.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+const size_t ARGS_COUNT = 3;
+
+const size_t R = 1;
+const size_t SQUARE_SIDE = 2;
+
+const double multCoeff = 4;
+
+using namespace std;
+
+struct Point {
+	Point(double x, double y) {
+		this->x = x;
+		this->y = y;
+	}
+	double x;
+	double y;
+};
+
+double GenerateRandomDouble(const double &min, const double &max)
+{	
+	return (double)(rand()) / RAND_MAX * (max - min) + min;
+}
+
+Point GenerateRandomPoint(size_t squareSize)
+{
+	double min = -(double)(squareSize / 2);
+	double max = (double)(squareSize / 2);
+	return Point(GenerateRandomDouble(min, max), GenerateRandomDouble(min, max));
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != ARGS_COUNT) {
+		cout << "Invalid count of arguements." << endl;
+		cout<< "Use: MonteCarloPi.exe <iterationsCount> <threadsCount>." << endl;
+		return 1;
+	}
+
+	size_t iterationsCount = atoi(argv[1]);	
+	size_t threadsCount = atoi(argv[2]);
+
+	size_t pointsInCircle = 0;
+
+	for(size_t i = 0; i < iterationsCount; i++) 
+	{
+		Point point = GenerateRandomPoint(SQUARE_SIDE);
+		if (point.x * point.x + point.y * point.y <= R)
+		{
+			pointsInCircle++;
+		}
+	}
+
+	double pi = multCoeff * pointsInCircle / iterationsCount;
+	cout << pi << endl;
+
+    return 0;
+}
